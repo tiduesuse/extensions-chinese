ab6a398471df0556b6afb122d0bf06429e16889b
tiduesusa <tiduesusa@gmail.com>
2022-01-14 14:57:55 -0500

    ver1.0.0 for BaoziManga

--- dev/BaoziManga/source.js
+++ dev/BaoziManga/source.js
@@ -404,7 +404,7 @@ const headers1 = {
 exports.BaoziMangaInfo = {
     version: '1.0.0',
     name: MG_NAME,
-    icon: 'icon.png',
+    icon: 'icon.jpg',
     author: 'Tomas Way',
     authorWebsite: 'https://github.com/Tiduesuse',
     description: 'Extension that pulls manga from ' + MG_NAME + ' (Chinese)',
