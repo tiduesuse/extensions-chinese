e321d0ad593b78b2419381593e70647800e30afd
tiduesusa <tiduesusa@gmail.com>
2022-01-14 11:22:27 -0500

    ver0.9.0 for BaoziManga

--- dev/BaoziManga/source.js
+++ dev/BaoziManga/source.js
@@ -511,15 +511,15 @@ class BaoziManga extends paperback_extensions_common_1.Source {
     }
     getHomePageSections(sectionCallback) {
         return __awaiter(this, void 0, void 0, function* () {
-            const section1 = createHomeSection({ id: '熱門漫畫', title: '熱門漫畫' });
-            const section2 = createHomeSection({ id: '推薦漫畫', title: '推薦漫畫' });
-            const section3 = createHomeSection({ id: '韓漫漫畫', title: '韓漫漫畫' });
-            const section4 = createHomeSection({ id: '大女主漫畫', title: '大女主漫畫' });
-            const section5 = createHomeSection({ id: '少年漫畫', title: '少年漫畫' });
-            const section6 = createHomeSection({ id: '戀愛漫畫', title: '戀愛漫畫' });
-            const section7 = createHomeSection({ id: '玄幻漫畫', title: '玄幻漫畫' });
-            const section8 = createHomeSection({ id: '最新上架', title: '最新上架' });
-            const section9 = createHomeSection({ id: '最近更新', title: '最近更新' });
+            const section1 = createHomeSection({ id: '熱門漫畫', title: '熱門漫畫', type: paperback_extensions_common_1.HomeSectionType.featured });
+            const section2 = createHomeSection({ id: '推薦漫畫', title: '推薦漫畫', view_more: true });
+            const section3 = createHomeSection({ id: '韓漫漫畫', title: '韓漫漫畫', view_more: true });
+            const section4 = createHomeSection({ id: '大女主漫畫', title: '大女主漫畫', view_more: true });
+            const section5 = createHomeSection({ id: '少年漫畫', title: '少年漫畫', view_more: true });
+            const section6 = createHomeSection({ id: '戀愛漫畫', title: '戀愛漫畫', view_more: true });
+            const section7 = createHomeSection({ id: '玄幻漫畫', title: '玄幻漫畫', view_more: true });
+            const section8 = createHomeSection({ id: '最新上架', title: '最新上架', view_more: true });
+            const section9 = createHomeSection({ id: '最近更新', title: '最近更新', view_more: true });
             const sections = [
                 section1,
                 section2,
